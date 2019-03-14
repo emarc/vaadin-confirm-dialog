9e4d402a0e9d24fb61d1863d33fe36d964b338a3
Marc Englund <marc@vaadin.com>
2019-03-14 15:12:58 +0200

    seed gh-pages

---
+++ components/vaadin-confirm-dialog/test/test-suites.js
@@ -0,0 +1,7 @@
+window.VaadinConfirmDialogSuites = [
+  'alert-api-test.html',
+  'cancel-api-test.html',
+  'confirmation-api-test.html',
+  'confirm-reject-api-test.html',
+  'remember-api-test.html'
+];
